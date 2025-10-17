cb57ba7484a25ab2ed52fff367408a709b5b017f
Sayfuddin Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2025-10-17 10:34:36 +0600

    Create and complete the apps not found page

--- src/pages/ErrorPage/AppsNotFound.jsx
+++ src/pages/ErrorPage/AppsNotFound.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import appError from "../../assets/images/App-Error.png"
+import { Link } from 'react-router';
 
 function AppsNotFound() {
   return (
-    <div>AppsNotFound</div>
+    <div className='containers bg-bg-body flex flex-col justify-center items-center gap-6 py-32'>
+      <figure>
+        <img src={appError} alt="apps not found icon" className='w-100'/>
+      </figure>
+      <p className="text-dark-04 font-semibold text-[2.8rem] lg:text-[4.8rem] text-center">
+        OOPS!! APP NOT FOUND
+      </p>
+      <p className="text-dark-05 text-[2rem]">
+        The App you are requesting is not found on our system.  please try another apps.
+      </p>
+      <Link to={'/apps'}>
+        <button className="btn-gradient cursor-pointer mt-12">Go Back</button>
+      </Link>
+
+    </div>
   )
 }
 
